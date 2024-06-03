5be3ecfd89d28b8e8981bec863fe9c85ec4e38d1
Katarina <queenkaca196@gmail.com>
2024-06-03 18:12:25 +0200

    prvi commit

---
+++ tetriss/Tabla.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tetriss
+{
+    internal class Tabla
+    {
+        #region atributi
+        int[,,] mreza;
+        int sirina;
+        int visina;
+        #endregion
+
+        #region konstruktori
+        public Tabla(int[,,] mreza, int sirina, int visina)
+        {
+            this.mreza = mreza;
+            this.sirina = sirina;
+            this.visina = visina;
+        }
+        
+        #endregion
+
+        #region svojstva
+        #endregion
+
+        #region metode
+        #endregion
+    }
+}
